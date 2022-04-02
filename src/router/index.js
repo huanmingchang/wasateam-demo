b975368b1a857c3085f6600f2775caf422311f4d
huanmingchang <huanming1988@gmail.com>
2022-04-02 13:22:19 +0800

    feat: project init

--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,30 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import Notfound from '../views/Notfound.vue'
 
-Vue.use(VueRouter);
+Vue.use(VueRouter)
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
+    path: '/',
+    name: 'rooy',
+    redirect: '/home',
+  },
+  {
+    path: '/home',
+    name: 'Home',
     component: Home,
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    path: '*',
+    name: 'Notfound',
+    component: Notfound,
   },
-];
+]
 
 const router = new VueRouter({
   routes,
-});
+})
 
-export default router;
+export default router
---
+++ src/views/Notfound.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>404 Not found</h1>
+</template>
+
+<style lang="scss" scoped>
+h1 {
+  margin-top: 4rem;
+  text-align: center;
+}
+</style>
