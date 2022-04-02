a03a34c60794abaa013d8dc4c8c50e51ea8e578c
huanmingchang <huanming1988@gmail.com>
2022-04-02 19:25:58 +0800

    feat: adjust component structure

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,94 @@
 <template>
-  <div id="app">
+  <div id="app" class="home" :class="{ theme_dark: mode === 'dark' }">
+    <h1 class="home__title">Wasateam Demo</h1>
+    <div class="home__subtitle">This is the demo page</div>
+    <div class="home__toggle">
+      <label for="light-mode" class="home__toggle__light">Light</label>
+      <input
+        type="radio"
+        name="mode"
+        class="home__toggle__light-input"
+        checked
+        value="light"
+        v-model="mode"
+      />
+      <label for="dark-mode" class="home__toggle__light">Dark</label>
+      <input
+        type="radio"
+        name="mode"
+        class="home__toggle__dark-input"
+        value="dark"
+        v-model="mode"
+      />
+    </div>
     <router-view />
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      mode: 'light',
+    }
+  },
+}
+</script>
 
 <style lang="scss">
 #app {
   width: 100vw;
   height: 100vh;
   background: var(--background);
+}
+
+.home {
+  display: flex;
+  flex-direction: column;
+  justify-items: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  padding: 2rem;
+  margin: 0 auto;
+  background: var(--background);
+
+  &__title {
+    width: 100%;
+    text-align: center;
+    margin-bottom: 0.5rem;
+    font-size: 2rem;
+    font-weight: 700;
+    color: var(--font-color);
+  }
+
+  &__subtitle {
+    width: 100%;
+    text-align: center;
+    font-size: 1rem;
+    font-weight: 400;
+    color: var(--secondary-font-color);
+  }
+
+  &__toggle {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    margin-top: 1rem;
+
+    label {
+      font-size: 0.8rem;
+      color: var(--font-color);
+    }
+
+    &__light-input,
+    &__dark-inpout {
+      margin-left: 0.5rem;
+    }
+    &__light-input {
+      margin-right: 2rem;
+    }
+  }
 }
 </style>
