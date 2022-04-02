f94c33211511305c37033d388a0e4a99d97301a1
huanmingchang <huanming1988@gmail.com>
2022-04-02 20:45:03 +0800

    feat: add textInput component and styles

---
+++ src/components/TextInput.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="input-section">
+    <label for="" class="input-section__label">Example</label>
+    <input
+      type="text"
+      class="input-section__text-input"
+      placeholder="Type something..."
+      v-model="input"
+    />
+    <div class="input-section__display">{{ input }}</div>
+    <button class="button" @click.stop.prevent="clear">Clear</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Input',
+  data() {
+    return {
+      input: '',
+    }
+  },
+  methods: {
+    clear() {
+      this.input = ''
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.input-section {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: relative;
+  width: 100%;
+  height: 100%;
+  border: 1px solid var(--border);
+  border-radius: 4px;
+  padding: 1rem;
+
+  &__label {
+    font-size: 1rem;
+    font-weight: 400;
+    color: var(--font-color);
+    margin-bottom: 0.5rem;
+  }
+
+  &__text-input {
+    padding: 0.5rem;
+    font-size: 1rem;
+    font-weight: 400;
+    color: var(--font-color);
+    border: 1px solid var(--border);
+    border-radius: 4px;
+    background: var(--background);
+    &::placeholder {
+      font-size: 0.8rem;
+      font-weight: 400;
+      color: var(--secondary-font-color);
+    }
+  }
+
+  &__display {
+    width: 80%;
+    height: 80%;
+    margin-top: 1rem;
+    color: var(--font-color);
+    background: var(--background);
+  }
+}
+
+.button {
+  position: absolute;
+  bottom: 10px;
+  right: 10px;
+  color: var(--font-color);
+  cursor: pointer;
+  &:hover {
+    text-decoration: underline;
+    text-decoration-color: #ff6600;
+    color: #ff6600;
+  }
+}
+</style>
---
+++ src/views/Input.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="input">
+    <h1 class="input__title">Input</h1>
+    <div class="input__section">
+      <label for="" class="input__section__label">Text</label>
+      <input
+        type="radio"
+        name="type"
+        class="input__section__input text"
+        checked
+        value="text"
+        v-model="inputType"
+      />
+      <label for="" class="input__section__label">Radio</label>
+      <input
+        type="radio"
+        name="type"
+        class="input__section__input radio"
+        value="radio"
+        v-model="inputType"
+      />
+      <label for="" class="input__section__label">Select</label>
+      <input
+        type="radio"
+        name="type"
+        class="input__section__input"
+        value="select"
+        v-model="inputType"
+      />
+    </div>
+    <TextInput v-if="inputType === 'text'" />
+    <RadioInput v-if="inputType === 'radio'" />
+    <SelectInput v-if="inputType === 'select'" />
+  </div>
+</template>
+
+<script>
+import TextInput from './../components/TextInput.vue'
+import RadioInput from './../components/RadioInput.vue'
+import SelectInput from './../components/SelectInput.vue'
+
+export default {
+  name: 'Input',
+  components: {
+    TextInput,
+    RadioInput,
+    SelectInput,
+  },
+  data() {
+    return {
+      inputType: 'text',
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.input {
+  display: flex;
+  flex-direction: column;
+  justify-items: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  padding: 2rem;
+  margin: 0 auto;
+  background: var(--background);
+  &__title {
+    width: 100%;
+    text-align: center;
+    margin-bottom: 2rem;
+    text-align: center;
+    font-size: 1.5rem;
+    font-weight: 700;
+    color: var(--font-color);
+  }
+
+  &__section {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    margin-bottom: 1rem;
+    label {
+      font-size: 0.8rem;
+      color: var(--font-color);
+    }
+
+    input {
+      margin-left: 0.5rem;
+      cursor: pointer;
+    }
+
+    .radio,
+    .text {
+      margin-right: 2rem;
+    }
+  }
+}
+</style>
